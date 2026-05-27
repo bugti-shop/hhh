5875530fc72e371c6508b692a6fb629d8b2bdb8a
bugti-shop <127424091+bugti-shop@users.noreply.github.com>
2026-05-27 08:17:11 +0500

    Change appId from 'nota.npd.com' to 'com.flowist.app'

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'nota.npd.com',
+  appId: 'com.flowist.app',
   appName: 'Flowist',
   webDir: 'dist',
   server: {
