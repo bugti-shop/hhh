a60f8784e13c4aae779a77d4243af14d3e2ba6dc
bugti-shop <127424091+bugti-shop@users.noreply.github.com>
2026-05-30 15:25:11 +0500

    Remove LocationRemindersMap from Upcoming component
    Removed lazy loading for LocationRemindersMap component.

--- src/pages/todo/Upcoming.tsx
+++ src/pages/todo/Upcoming.tsx
@@ -21,7 +21,7 @@ import { PrioritySelectSheet } from '@/components/PrioritySelectSheet';
 import { TaskItem } from '@/components/TaskItem';
 import { SmartListsDropdown, SmartListType, getSmartListFilter } from '@/components/SmartListsDropdown';
 import { lazy, Suspense } from 'react';
-const LocationRemindersMap = lazy(() => import('@/components/LocationRemindersMap').then(m => ({ default: m.LocationRemindersMap })));
+ 
 
 import { Haptics, ImpactStyle } from '@capacitor/haptics';
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
@@ -715,15 +715,7 @@ const Upcoming = () => {
         onSelect={handleSetPriority}
       />
       
-      {isLocationMapOpen && (
-        <Suspense fallback={null}>
-          <LocationRemindersMap
-            open={isLocationMapOpen}
-            onOpenChange={setIsLocationMapOpen}
-            tasks={items}
-          />
-        </Suspense>
-      )}
+       
       
     </TodoLayout>
   );
